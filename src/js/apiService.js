d262d412576747d7cca608534fdce102b7d22f2a
IhorShvydkyi <igorshvydky35@gmail.com>
2021-11-04 21:37:12 +0200

    install parcel

---
+++ src/js/apiService.js
@@ -0,0 +1,3 @@
+const KEY = '24190163-4fef3af5a8c9fb39e43dd5d2d';
+const BASE_URL = 'https://pixabay.com/api/?image_type=photo&orientation=horizontal&';
+
--- src/js/fetchCountries.js
+++
@@ -1,8 +0,0 @@
-const BASE_URL = 'https://restcountries.com'
-
-function fetchCountries(text) {
-    return  fetch(`${BASE_URL}/v2/name/${text}`).then(response =>
-        response.json(),
-        );
-}
-export default { fetchCountries };
